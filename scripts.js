68d6c2ce28242c3bf426a453c799eff51111dc5e
Nathaniel Jew <nathankjew@gmail.com>
2019-10-27 00:51:11 -0700

    Color stuff and equal rhymes

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 var lines = [""];
 var rhymes = [];
 var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var rhymeColors = [];
+var colorValNum = 0;
+var colorValDenom = 1;
 
 var lineTemplate = "<div class=\"line-container\"><input class=\"inputbox\" type=\"text\" oninput=\"dealWithKeyboard(#)\" onchange=\"dealWithEnd(#)\" autocomplete=\"off\" id=\"input-#\" placeholder=\". . .\"><p class=\"syllable-counter\" id=\"counter-#\">0</p><p class=\"rhymer\" id=\"rhymer-#\">0</p></div>";
 
@@ -38,8 +41,9 @@ function dealWithKeyboard(id) {
 
 function updateRhymeDisplay() {
     for (var i = 0; i < lines.length; i++) {
-        var display = (lines[i] !== "") ? rhymes[i] : "";
+        var display = (lines[i] !== "") ? alphabet[rhymes[i]] : "";
         document.getElementById("rhymer-" + i).innerHTML = display;
+        // document.getElementById("rhymer-" + i).style.color = "#" + getColor(rhymeColors[rhymes[i]]);
     }
 }
 
@@ -67,7 +71,9 @@ function updateAllRhymes() {
 
 function updateLineRhyme(id) {
     for (var i = 0; i < rhymes.length; i++) {
-        if (RiTa.isRhyme(lines[id], lines[i])) {
+        curLastWord = lastWord(lines[id]);
+        otherLastWord = lastWord(lines[i]);
+        if (curLastWord.toLowerCase() === otherLastWord.toLowerCase() || RiTa.isRhyme(curLastWord, otherLastWord)) {
             rhymes.push(rhymes[i]);
             return;
         }
@@ -75,9 +81,14 @@ function updateLineRhyme(id) {
     rhymes.push(getNextRhyme());
 }
 
+function lastWord(line) {
+    var words = line.split(' ');
+    return words[words.length - 1];
+}
+
 function getNextRhyme() {
     if (rhymes.length == 0) {
-        return alphabet.charAt(0);
+        return 0;
     }
     var max = rhymes[0];
     for (var i = 0; i < rhymes.length; i++) {
@@ -85,5 +96,24 @@ function getNextRhyme() {
             max = rhymes[i];
         }
     }
-    return alphabet.charAt(alphabet.indexOf(max) + 1);
+
+    rhymeColors.push(colorValNum / colorValDenom);
+    colorValNum += 2;
+    if (colorValNum >= colorValDenom) {
+        colorValNum = 1;
+        colorValDenom *= 2;
+    }
+
+    return max + 1;
+}
+
+function getColor(value) {
+    r = Math.floor(clamp01(2 - (6 * (Math.abs(Math.round(value) - value)))) * 192);
+    g = Math.floor(clamp01(2 - (6 * (Math.abs(Math.round(value + (2/3)) - (value + (2/3)))))) * 192);
+    b = Math.floor(clamp01(2 - (6 * (Math.abs(Math.round(value + (1/3)) - (value + (1/3)))))) * 192);
+    return (r.toString(16).padStart(2, '0') + g.toString(16).padStart(2, '0') + b.toString(16).padStart(2, '0'));
+}
+
+function clamp01(value) {
+    return Math.min(Math.max(0, value), 1)
 }
